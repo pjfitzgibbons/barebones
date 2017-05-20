d1141a7eb136831d8cca681740a156cb6f2d146b
Peter Fitzgibbons <peter.fitzgibbons@gmail.com>
2017-05-19 21:59:33 -0700

    Prod logging configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,10 +49,10 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
